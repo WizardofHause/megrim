cbf0b39fa9e963ffacdba24bf96f13297ef63468
WizardofHause <jonrhause@gmail.com>
2023-03-01 16:20:44 -0700

    build: react-router-dom setup, cookies & sessions init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
     def hello_world
-        count = (count || 0) + 1
-        render json: { count: count }
+        session[:count] = (session[:count] || 0) + 1
+        render json: { count: session[:count] }
     end
 end
